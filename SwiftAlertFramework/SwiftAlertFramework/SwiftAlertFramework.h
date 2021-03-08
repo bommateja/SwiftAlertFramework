707a5f97006b41d17320211cc10c0b23fc363849
Theja <theja@biocipher.in>
2021-03-08 14:05:45 +0530

    finesh our SwiftAlertFramework

---
+++ SwiftAlertFramework/SwiftAlertFramework/SwiftAlertFramework.h
@@ -0,0 +1,18 @@
+//
+//  SwiftAlertFramework.h
+//  SwiftAlertFramework
+//
+//  Created by apple on 08/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftAlertFramework.
+FOUNDATION_EXPORT double SwiftAlertFrameworkVersionNumber;
+
+//! Project version string for SwiftAlertFramework.
+FOUNDATION_EXPORT const unsigned char SwiftAlertFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAlertFramework/PublicHeader.h>
+
+
